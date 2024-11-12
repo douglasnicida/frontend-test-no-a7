72fe71e51456ffb831c4fb8bcd4e3b71cebbd4ec
Douglas Nicida <douglasnicida@gmail.com>
2024-11-12 17:42:06 -0300

    [feat] - test completed

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.static('public'));
+app.set("views", path.join(__dirname, "../views")); 
+app.use(express.static('../public'));
 app.use(express.json());
 
 let items = []
